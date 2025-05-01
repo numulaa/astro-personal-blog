70d933b48ec96f7aab68ad121c9453022b534f52
Nurul Mukhlisa <numulaa@gmail.com>
2025-05-02 00:28:32 +0800

    added images to my 1st blog post

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,21 +1,28 @@
-import rss from '@astrojs/rss';
-import {getCollection} from 'astro:content';
-import { useTranslations } from '@/i18n';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { useTranslations } from "@/i18n";
 
 const t = useTranslations();
 
 export async function GET(context) {
-    const posts = await getCollection('blog');
-    return rss({
-        title: t('siteMetadata.title'),
-        description: t('siteMetadata.description'),
-        site: context.site,
-        items: posts.map(({slug, data: {title, summary, tags, date}}) => ({
-            title,
-            categories: tags.map(({slug}) => slug), // TODO: add tags name in the future
-            pubDate: date,
-            description: summary,
-            link: `/blog/${slug}/`,
-        })),
-    });
+  const posts = await getCollection("blog");
+  // Sort posts by date
+  //   const items = [...blog].sort(
+  //     (a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf()
+  //   );
+  return rss({
+    title: t("siteMetadata.title"),
+    description: t("siteMetadata.description"),
+    site: context.site,
+    items: posts.map(({ slug, data: { title, summary, tags, date } }) => {
+      const parts = slug.split("/").pop();
+      return {
+        title,
+        categories: tags.map(({ slug }) => slug), // TODO: add tags name in the future
+        pubDate: date,
+        description: summary,
+        link: `/blog/${parts}/`,
+      };
+    }),
+  });
 }
