3a35cc56bc0565f33d35e3fb822926635dade06d
Nurul Mukhlisa <numulaa@gmail.com>
2025-06-25 19:48:00 +0800

    updated projects

--- src/consts.ts
+++ src/consts.ts
@@ -104,7 +104,7 @@ export const ITEMS_PER_PAGE = 5;
 export const NAVIGATION = [
   { href: "/", title: "nav.home" },
   { href: "/blog", title: "nav.blog" },
-  { href: "/tags", title: "nav.tags" },
+  // { href: "/tags", title: "nav.tags" },
   { href: "/projects", title: "nav.projects" },
   { href: "/about", title: "nav.about" },
 ] as const;
